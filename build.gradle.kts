90cdc24fd8419b75f84be27bf65b1b4d965284d5
Michael Zh <mikooomichael@gmail.com>
2025-04-19 13:51:23 -0400

    Migrate to module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,46 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-	alias(libs.plugins.android.application) apply false
-	alias(libs.plugins.kotlin.android) apply false
+	id("com.android.library")
+	kotlin("android")
+}
+
+kotlin {
+	jvmToolchain(17)
+}
+
+android {
+	namespace = "wah.mikooomich.ffMetadataEx"
+	compileSdk = 35
+
+	defaultConfig {
+		minSdk = 24
+
+		externalNativeBuild {
+			cmake {
+				arguments += listOf("-DCMAKE_SHARED_LINKER_FLAGS=-Wl,--build-id=none")
+			}
+		}
+	}
+
+	sourceSets {
+		getByName("main") {
+			jniLibs.srcDirs("ffmpeg-android-maker/output/lib/")
+		}
+	}
+
+	externalNativeBuild {
+		cmake {
+			path = file("src/main/cpp/CMakeLists.txt")
+			version = "3.31.6"
+		}
+	}
+
+	ndkVersion = "29.0.13113456"
+
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_17
+		targetCompatibility = JavaVersion.VERSION_17
+	}
+	kotlinOptions {
+		jvmTarget = "17"
+	}
 }
